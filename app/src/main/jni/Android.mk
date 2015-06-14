c8c639c0d68883481b58a5f66d4710d9a1095ebe
Pluu <pluulove@gmail.com>
2015-06-14 13:52:44 +0900

    1. Step 2 Commit

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -3,8 +3,18 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_DEFAULT_CPP_EXTENSION := cpp
+
 LOCAL_MODULE := test_google_breakpad
 LOCAL_SRC_FILES := test_breakpad.cpp
+LOCAL_STATIC_LIBRARIES += breakpad_client
 LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
 
 include $(BUILD_SHARED_LIBRARY)
+
+# If NDK_MODULE_PATH is defined, import the module, otherwise do a direct
+# includes. This allows us to build in all scenarios easily.
+ifneq ($(NDK_MODULE_PATH),)
+  $(call import-module,google_breakpad)
+else
+  include $(LOCAL_PATH)/google_breakpad/Android.mk
+endif
--- app/src/main/jni/test_breakpad.cpp
+++ app/src/main/jni/test_breakpad.cpp
@@ -1,16 +1,39 @@
 #include <jni.h>
-#include <stdio.h>
 #include <android/log.h>
+#include <stdio.h>
+
+#include "google_breakpad/src/client/linux/handler/exception_handler.h"
+#include "google_breakpad/src/client/linux/handler/minidump_descriptor.h"
+
+static google_breakpad::ExceptionHandler* exceptionHandler;
+bool DumpCallback(const google_breakpad::MinidumpDescriptor& descriptor,
+                  void* context,
+                  bool succeeded) {
+
+    __android_log_print(ANDROID_LOG_DEBUG, "PluuSystem", "Dump path: %s\n", descriptor.path());
+    return succeeded;
+}
+
+void Crash() {
+    volatile int* a = reinterpret_cast<volatile int*>(NULL);
+    *a = 1;
+}
 
 extern "C" {
     void Java_com_pluusystem_breakpadjavacall_MainActivity_initNative(JNIEnv* env, jobject obj, jstring filepath)
     {
+        const char *path = env->GetStringUTFChars(filepath, 0);
+        google_breakpad::MinidumpDescriptor descriptor(path);
+        exceptionHandler = new google_breakpad::ExceptionHandler(descriptor, NULL, DumpCallback, NULL, true, -1);
+
         __android_log_print(ANDROID_LOG_DEBUG, "PluuSystem", "initNative cal");
     }
 
     void Java_com_pluusystem_breakpadjavacall_MainActivity_crashService(JNIEnv* env, jobject obj)
     {
-        __android_log_print(ANDROID_LOG_DEBUG, "PluuSystem", "crashService call");
+    	__android_log_print(ANDROID_LOG_DEBUG, "PluuSystem", "crashService call");
+
+        Crash();
     }
 
 }
